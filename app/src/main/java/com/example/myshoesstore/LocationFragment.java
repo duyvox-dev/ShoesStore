bd6f282be49cf43913f779734b19c3d7bb1364fa
hung-nv95 <hungnvse151468@fpt.edu.vn>
2023-07-12 13:24:55 +0700

    location & phone

--- app/src/main/java/com/example/myshoesstore/LocationFragment.java
+++ app/src/main/java/com/example/myshoesstore/LocationFragment.java
@@ -1,32 +1,60 @@
 package com.example.myshoesstore;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LocationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class LocationFragment extends Fragment {
+import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap myMap;
+    private ImageView imgCall;
 
     public LocationFragment() {
         // Required empty public constructor
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+
+        // Get a handle to the fragment and register the callback.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        ImageView imgCall = view.findViewById(R.id.img_call);
+        imgCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:0987654321"));
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+
+    // Get a handle to the GoogleMap object and display marker.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        myMap = googleMap;
+        // set mức zoom từ 2.0 tới 21.0
+        float zoomLevel = 15.0f;
+        LatLng store = new LatLng(10.8421301, 106.8100118);
+        myMap.addMarker(new MarkerOptions().position(store).title("Store"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(store, zoomLevel));
     }
 }
